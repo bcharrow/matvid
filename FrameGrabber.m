6e82f409ca13efc1afaafc3f2bd94d44c71325db
Matthew Turpin <mturpin@seas.upenn.edu>
2012-09-27 14:57:33 -0400

    Added the option to grab every N frames

--- FrameGrabber.m
+++ FrameGrabber.m
@@ -9,6 +9,7 @@ classdef FrameGrabber < handle
         transparency = false; % If true, preserve transparencies when grabbing
         fmt = '%07d';         % Format string for index suffix
         overwrite = false;    % If true, overwrite existing imageswhen grabbing
+        grabevery = 1;        % Saves every grabevery frames to disk
     end
     
     methods(Static)
@@ -41,7 +42,7 @@ classdef FrameGrabber < handle
             obj.fh = fh;
             obj.path = attr.Name;
             obj.prefix = prefix;
-        end              
+        end
         
         function [] = set.transparency(obj, trans)
             if ~islogical(trans) && trans ~= 1 && trans ~= 0
@@ -49,7 +50,7 @@ classdef FrameGrabber < handle
             end
             obj.transparency = logical(trans);
         end
-
+        
         function [] = set.overwrite(obj, ov)
             if ~islogical(ov) && ov ~= 1 && ov ~= 0
                 error('overwrite must be true/false/0/1');
@@ -57,7 +58,7 @@ classdef FrameGrabber < handle
             obj.overwrite = logical(ov);
         end
         
-        function [] = set.fh(obj, fh)            
+        function [] = set.fh(obj, fh)
             if  length(fh) ~= 1 || ~ishandle(fh)
                 error('Must specify single valid file handle');
             end
@@ -68,47 +69,51 @@ classdef FrameGrabber < handle
             % Delete all files associated with path and prefix
             suffixes = {'.pdf', '.jpg', '.eps', '.svg'};
             glob = sprintf('%s/%s*', obj.path, obj.prefix);
-            cellfun(@(suffix) delete([glob suffix]), suffixes);            
+            cellfun(@(suffix) delete([glob suffix]), suffixes);
         end
         
         function [] = grab(obj)
-            % Save figure to disk as a PDF
+            % Save figure to disk as a PDF if internal index is multiple of
+            % grabevery
             %
             % -Uses current value of ind to add a suffix index
             % -If overwrite is false, will not delete existing file
-            if ~isempty(obj.prefix)
-                fmt_str = ['%s/%s' obj.fmt];
-                fname = sprintf(fmt_str, obj.path, obj.prefix, obj.ind);
-            else
-                fmt_str = ['%s/' obj.fmt];
-                fname = sprintf(fmt_str, obj.path, obj.ind);
-            end
             
-            % Check if result already exists
-            pdf = [fname '.pdf'];
-            if ~obj.overwrite && exist(pdf, 'file')
-                error('File %s already exists', pdf);
-            end
-            
-            % Grab file and produce PDF
-            if ~obj.transparency
-                eps = [fname '.eps'];
-                print(obj.fh, eps, '-depsc');
-                cmd = sprintf('epstopdf %s', eps);
-                obj.system(cmd);
-                delete(eps);
-            else
-                svg = [fname '.svg'];
-                % Suppress annoying output from plot2svg
-                text_output = evalc('plot2svg(svg, obj.fh)');
-                if ~strcmp(text_output(4:41),'Matlab/Octave to SVG converter version')
-                    disp(text_output);
+            if (obj.grabevery > 0)  &&  (mod(obj.ind,obj.grabevery) == 0)
+                
+                if ~isempty(obj.prefix)
+                    fmt_str = ['%s/%s' obj.fmt];
+                    fname = sprintf(fmt_str, obj.path, obj.prefix, obj.ind);
+                else
+                    fmt_str = ['%s/' obj.fmt];
+                    fname = sprintf(fmt_str, obj.path, obj.ind);
                 end
-                cmd = sprintf('rsvg-convert -f pdf -o %s %s', pdf, svg);
-                obj.system(cmd)
-                delete(svg);
+                
+                % Check if result already exists
+                pdf = [fname '.pdf'];
+                if ~obj.overwrite && exist(pdf, 'file')
+                    error('File %s already exists', pdf);
+                end
+                
+                % Grab file and produce PDF
+                if ~obj.transparency
+                    eps = [fname '.eps'];
+                    print(obj.fh, eps, '-depsc');
+                    cmd = sprintf('epstopdf %s', eps);
+                    obj.system(cmd);
+                    delete(eps);
+                else
+                    svg = [fname '.svg'];
+                    % Suppress annoying output from plot2svg
+                    text_output = evalc('plot2svg(svg, obj.fh)');
+                    if ~strcmp(text_output(4:41),'Matlab/Octave to SVG converter version')
+                        disp(text_output);
+                    end
+                    cmd = sprintf('rsvg-convert -f pdf -o %s %s', pdf, svg);
+                    obj.system(cmd)
+                    delete(svg);
+                end
             end
-            
             obj.ind = obj.ind + 1;
         end
     end
@@ -116,9 +121,9 @@ classdef FrameGrabber < handle
     methods(Static, Access = private)
         function [] = system(cmd)
             [stat, result] = system(cmd);
-            if stat ~= 0 
+            if stat ~= 0
                 error('Error executing "%s"\n%s', cmd, result);
-            end                
+            end
         end
     end
     
